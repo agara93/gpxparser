9e22e557358dbc065546adaa22f02b23772ece95
fbrandy <adit293@ymail.com>
2015-08-11 10:17:24 +0000

    add routes

--- parser.rb
+++ parser.rb
@@ -1,7 +1,9 @@
 require 'nokogiri'
- 
-      file = File.open("gpx.xml")
-      doc = Nokogiri::XML(file)
+
+    #waypoints
+    
+      fileway = File.open("waypoints.xml")
+      doc = Nokogiri::XML(fileway)
       doc.remove_namespaces!
       puts " "
       puts "======================================"
@@ -12,36 +14,95 @@ require 'nokogiri'
       puts "=========="
       puts "Waypoints"
       puts "=========="
-      trackpoints = doc.xpath('//wpt[@*]')
+
+      waypoints = doc.xpath('//wpt[@*]')
       count = 0
-      trackpoints.each do |trkpt|
+      waypoints.each do |wpt|
             count += 1
             puts " "
-            puts "id 	   = #{count}"
-            puts "lat 	   = " + trkpt.at_xpath('@lat')
-            puts "lon 	   = " + trkpt.at_xpath('@lon')
-            if elev = trkpt.at_css('/ele')
+            puts "wpt id     = #{count}"
+            puts "lat 	   = " + wpt.at_xpath('@lat')
+            puts "lon 	   = " + wpt.at_xpath('@lon')
+            if elev = wpt.at_css('/ele')
                 puts "ele 	   = #{elev.text}"
             else
-                trkpt.next
+                wpt.next
             end
-            if name = trkpt.at_css('/name')
+            if name = wpt.at_css('/name')
                 puts "name 	   = #{name.text}"
             else
-                trkpt.next
+                wpt.next
             end
-            if desc = trkpt.at_css('/desc')
+            if desc = wpt.at_css('/desc')
                 puts "desc 	   = #{desc.text}"
             else
-                trkpt.next
+                wpt.next
             end
-            if time = trkpt.at_css('/time')
+            if time = wpt.at_css('/time')
                 puts "created at = #{time.text}"
             else
                 print " "
             end
             puts " "
-      end
+         end
+         
+        # routes
+        
+        filerte = File.open("routes.xml")
+        docrte = Nokogiri::XML(filerte)
+        docrte.remove_namespaces!
+        puts " "
+        puts "=========="
+        puts "  Routes"
+        puts "=========="
+        
+        route = docrte.xpath('//rte')
+        countrte = 0
+        route.each do |rte|
+            countrte +=1
+            puts " "
+            puts rte.at_css('/name').text 
+            puts "========================"
+            puts rte.at_css('/desc').text
+            puts " "
+        
+            routes = docrte.xpath('//rtept[@*]')
+            countrtept = 0
+            routes.each do |rtept|
+                countrtept +=1
+                puts "rte id     = #{countrtept}"
+                puts "lat 	   = " + rtept.at_xpath('@lat')
+                puts "lon 	   = " + rtept.at_xpath('@lon')
+                if elev = rtept.at_css('/ele')
+                    puts "ele 	   = #{elev.text}"
+                else
+                    rtept.next
+                end
+                if name = rtept.at_css('/name')
+                    puts "name 	   = #{name.text}"
+                else
+                    rtept.next
+                end
+                if desc = rtept.at_css('/desc')
+                    puts "desc 	   = #{desc.text}"
+                else
+                    rtept.next
+                end
+                if time = rtept.at_css('/time')
+                    puts "created at = #{time.text}"
+                else
+                    print " "
+                end
+                puts " "
+             end
+         end
+        
+        puts " ======================"
+        puts "    End of data     "
+        puts " #{count} Waypoints "
+        puts " #{countrte} Routes "
+        puts " ======================"
+        puts " "
  
      
 
